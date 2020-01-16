d73a242077a3c162419ba6b8781b059cb302f483
Giovanni Garcia <ggarcia@nearsoft.com>
2020-01-16 00:23:20 +0000

    complete unit test for list component

--- src/app/gif-list/gif-list.component.spec.ts
+++ src/app/gif-list/gif-list.component.spec.ts
@@ -11,14 +11,20 @@ describe('GifListComponent', () => {
     let fixture: ComponentFixture<GifListComponent>;
     const mockImageProviderService = jasmine.createSpyObj<ImageProvider<GifModel>>('ImageProvider', ['provide']);
     let imgArray: HTMLImageElement[] = [];
-    beforeEach(async(() => {
+
+    const initServices = (): void => {
         mockImageProviderService.provide.and.callFake((resource) => {
             const image = new Image();
             image.src = resource.src;
             image.title = resource.title;
+            image.className = 'img-test';
             imgArray = [].concat(...imgArray, image);
             return image;
         });
+    };
+
+    beforeEach(async(() => {
+        initServices();
         TestBed.configureTestingModule({
             providers: [
                 Renderer2,
@@ -34,46 +40,68 @@ describe('GifListComponent', () => {
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
+
     it('should create', () => {
         expect(component).toBeTruthy();
     });
-    fit('load images', () => {
+
+    it('should show loading message when loading is active and keep hide card-list element', () => {
+        const expectedMessage = 'Loading...';
+
+        component.isLoadingContent = true;
+        fixture.detectChanges();
+
+        const loadingMessage = fixture.debugElement.query(By.css('#loading-msessage'));
+        const cardList = fixture.debugElement.query(By.css('#card-list'));
+
+        expect(loadingMessage.nativeElement.innerHTML).toEqual(expectedMessage);
+        expect(cardList.styles.display).toEqual('none');
+    });
+
+    it('should show card-list with images, and hide loading-message when receive a gifArray', async () => {
+        const gifArray = [
+            {
+                url: 'urlOne',
+                title: 'titleOne',
+                src: 'imageUrlOne'
+            },
+            {
+                url: 'urlTwo',
+                title: 'titleTwo',
+                src: 'imageUrlTwo'
+            },
+            {
+                url: 'urlThree',
+                title: 'titleThree',
+                src: 'imageUrlThree'
+            },
+            {
+                url: 'urlFour',
+                title: 'titleFour',
+                src: 'imageUrlFour'
+            },
+            {
+                url: 'urlFive',
+                title: 'titleFive',
+                src: 'imageUrlFive'
+            }
+        ];
+
         const images$ = cold('-a|', {
-            a: [
-                {
-                    url: 'urlOne',
-                    title: 'titleOne',
-                    src: 'imageUrlOne'
-                },
-                {
-                    url: 'urlTwo',
-                    title: 'titleTwo',
-                    src: 'imageUrlTwo'
-                },
-                {
-                    url: 'urlThree',
-                    title: 'titleThree',
-                    src: 'imageUrlThree'
-                },
-                {
-                    url: 'urlFour',
-                    title: 'titleFour',
-                    src: 'imageUrlFour'
-                },
-                {
-                    url: 'urlFive',
-                    title: 'titleFive',
-                    src: 'imageUrlFive'
-                }
-            ]
+            a: gifArray
         });
+
         component.imageList = images$;
+
         getTestScheduler().flush();
         imgArray.forEach(img => img.dispatchEvent(new Event('load')));
-
-        fixture.detectChanges();
         getTestScheduler().flush();
+        fixture.detectChanges();
+
         const cardList = fixture.debugElement.query(By.css('#card-list'));
-        console.log(cardList)
+        const loadingMessage = fixture.debugElement.query(By.css('#loading-msessage'));
+
+        expect([...cardList.nativeElement.querySelectorAll('img.img-test')].length).toEqual(gifArray.length);
+        expect(loadingMessage).toBeFalsy();
     });
 });
